54f9f53806e8093b0c1d0ae7904ddc1301c992fa
Csengeri Marcell <diak@petrik.hu>
2026-09-14 10:33:27 +0200

    Rat form done.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import './style.css'
 import { ElectricRat } from './ElectricRat.ts'
 
+const rat: ElectricRat = new ElectricRat("Fernando");
+console.log(rat.name, rat.attack, rat.health);
+
+function CreateNewRat(event: SubmitEvent) {
+  event.preventDefault();
+  const ratForm = document.getElementById("RAT_FORM") as HTMLFormElement;
+  const ratName = document.getElementById("RAT_FORM_NAME") as HTMLInputElement;
+  const newRat: ElectricRat = new ElectricRat(ratName.value);
+  console.log(newRat.name, newRat.attack, newRat.health);
+  ratForm.reset();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded 😊");
+  document.getElementById("RAT_FORM")?.addEventListener("submit", CreateNewRat);
+})

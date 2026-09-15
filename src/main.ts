885b4106d2f334a58734bae360479b03dbd779a8
Petrik Diák <diak@petrik.hu>
2026-09-15 13:51:20 +0200

    Style and listing is done and finished.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import './style.css'
 import { ElectricRat } from './ElectricRat.ts'
 
+const eRats: ElectricRat[] = [];
 const rat: ElectricRat = new ElectricRat("Fernando");
 console.log(rat.name, rat.attack, rat.health);
+eRats.push(rat);
 
 function CreateNewRat(event: SubmitEvent) {
   event.preventDefault();
@@ -10,10 +12,38 @@ function CreateNewRat(event: SubmitEvent) {
   const ratName = document.getElementById("RAT_FORM_NAME") as HTMLInputElement;
   const newRat: ElectricRat = new ElectricRat(ratName.value);
   console.log(newRat.name, newRat.attack, newRat.health);
+  // alert("New rat has been added! 🐀")
+  eRats.push(newRat);
   ratForm.reset();
+  ListRats ()
+}
+
+function ListRats () {
+  const cards = document.getElementById("CARDS") as HTMLDivElement;
+  cards.innerHTML = "";
+
+  eRats.forEach(eRat => {
+    console.log("Listed");
+    const card = document.createElement("div");
+    cards.appendChild(card);
+
+    const pName = document.createElement("p");
+    pName.innerText = `${eRat.name}`;
+    card.appendChild(pName);
+
+    const pAttack = document.createElement("p");
+    pAttack.innerText = `ATK: ${eRat.attack}⚔️`;
+    card.appendChild(pAttack);
+
+    const pHealth = document.createElement("p");
+    pHealth.innerText = `HP: ${eRat.health}💖`;
+    card.appendChild(pHealth);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded 😊");
   document.getElementById("RAT_FORM")?.addEventListener("submit", CreateNewRat);
+
+  ListRats();
 })
